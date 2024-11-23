f2861dc60bfe254efec62daddc0b5ce6e5f1cc6c
JiangHongAiBianCheng <jiang_hong_like_5@qq.com>
2024-11-23 21:18:22 +0800

    Add files via upload

---
+++ js/bf.js
@@ -0,0 +1,72 @@
+function clearoutput() {
+    document.getElementById("output").value = "";
+}
+
+function runcode() {
+    var code = getcode();
+    var output = run(code);
+    document.getElementById("output").value = output;
+}
+
+function getcode() {
+    var code = document.getElementById("code").value;
+    return code;
+}
+
+function run(code) {
+    let codePnt = 0;
+    let memPnt = 0;
+    let output = '';
+    let mem_len = 30000;
+    let mem = new Array(mem_len).fill(0);
+
+    while (codePnt < code.length) {
+    switch (code[codePnt]) {
+        case '>':
+        if (memPnt < mem.length) {
+            memPnt++;
+        } else {
+            console.warn('Out of bounds');
+        }
+        break;
+        case '<':
+        if (memPnt > 0) {
+            memPnt--;
+        } else {
+            console.warn('Out of bounds');
+        }
+        break;
+        case '+':
+        mem[memPnt]++;
+        break;
+        case '-':
+        mem[memPnt]--;
+        break;
+        case '.':
+        output += String.fromCharCode(mem[memPnt]);
+        break;
+        case ',':
+        var input = prompt();
+        mem[memPnt] = input.charCodeAt(0);
+        break;
+        case '[':
+        while (mem[memPnt] === 0) {
+            codePnt++;
+            if (code[codePnt] === ']') {
+            break;
+            }
+        }
+        break;
+        case ']':
+        while (mem[memPnt] !== 0) {
+            codePnt--;
+            if (code[codePnt] === '[') {
+            break;
+            }
+        }
+        break;
+    }
+    codePnt++;
+    }
+    return output;
+}
